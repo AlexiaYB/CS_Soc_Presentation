512f10809deb71b61de16a1de540eaa393241852
AlexiaYB <95047087+AlexiaYB@users.noreply.github.com>
2023-09-19 16:40:41 +0100

    Add files via upload

---
+++ caesar_cipher_exercise.py
@@ -0,0 +1,6 @@
+# text 1 is plaintext to encrypt into ciphertext
+text1 = "alicewasbeginningtogetverytiredofsittingbyhersisteronthebankandofhavingnothingtodoonceortwiceshehadpeepedintothebookhersisterwasreadingbutithadnopicturesorconversationsinitandwhatistheuseofabookthoughtalicewithoutpicturesorconversations"
+# text 2 is ciphertext to decrypt into plaintext
+text2 = "ufueoqdfmuzxkdmftqdmogduagebdapgofuazemuptaxyqemfrudefeustfufiagxpmbbqmdfanqeayqotuxpuetbdmzwufoazeuefearmzgynqdarmnegdpxuffxqrusgdqepmzouzsmodaeeftqbmbqdgbazituotftqkmdqpdmizitketagxpkagmffdungfqmzkuybadfmzoqfaeasdafqecgqmzanvqof"
+# alphabet for you to reference
+alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
